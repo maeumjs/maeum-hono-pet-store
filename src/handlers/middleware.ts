29d0f96ddf44c5ba5e4eb69f907b1e0deabe7415
ByungJoon Lee <imjuni@gmail.com>
2026-02-27 00:36:30 +0900

    refactor: streamline middleware initialization by moving hooks to middleware function

--- src/handlers/middleware.ts
+++ src/handlers/middleware.ts
@@ -2,10 +2,17 @@ import { serveStatic } from '@hono/node-server/serve-static';
 import { requestId } from 'hono/request-id';
 
 import { container } from '#/loader';
+import { defaultHook } from '#/modules/hook/default.hook';
+import { onError } from '#/modules/hook/on.error';
 import { asyncContextMiddleware } from '#/modules/middleware/async.context.middleware';
 import { httpLoggingMiddleware } from '#/modules/middleware/http.logging.middleware';
 
 export function middleware(): void {
+  // configuration default hook
+  container.app.defaultHook = defaultHook;
+  // add hook for error processing
+  container.app.onError(onError);
+
   container.app.use('*', requestId());
   container.app.use('/static/*', serveStatic({ root: './public' }));
 
--- src/modules/initialize/init.app.ts
+++ src/modules/initialize/init.app.ts
@@ -1,12 +1,7 @@
 import { OpenAPIHono } from '@hono/zod-openapi';
 
-import { defaultHook } from '#/modules/hook/default.hook';
-import { onError } from '#/modules/hook/on.error';
-
 export function initApp(): OpenAPIHono {
-  const app = new OpenAPIHono({ defaultHook });
-
-  app.onError(onError);
+  const app = new OpenAPIHono();
 
   return app;
 }
