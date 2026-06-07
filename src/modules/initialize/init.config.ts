f94eacd9daed574375f9986d08a48a280d18d0d6
ByungJoon Lee <imjuni@gmail.com>
2026-06-07 15:04:28 +0900

    refactor: update nodemon exec command and add dev-script to package.json - configure default formatter for various file types in VSCode settings - remove prettier from dependencies

--- src/modules/initialize/init.config.ts
+++ src/modules/initialize/init.config.ts
@@ -7,9 +7,10 @@ import { ConfigurationSchema } from "#schema/configuration/configuration.zod.js"
 import { ServerConfigurationSchema } from "#schema/configuration/server.zod.js";
 
 export async function initConfig(): Promise<z.infer<typeof ConfigurationSchema>> {
+  const extname = "jsonc";
   const dirname = path.join(process.cwd(), "resources", "configs");
   const runMode = ServerConfigurationSchema.shape.runMode.parse(process.env.RUN_MODE);
-  const filename = `config.${runMode}.json`;
+  const filename = `config.${runMode}.${extname}`;
   const configBuf = await fs.promises.readFile(path.join(dirname, filename));
   const parsed = parse(configBuf.toString());
   const valiated = ConfigurationSchema.parse(parsed);
